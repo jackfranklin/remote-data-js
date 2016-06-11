7359e1c29bba8b1d33b81b2ab85431ef9146b297
Jack Franklin <jack@jackfranklin.net>
2016-06-11 13:13:28 +0100

    Add template for a Redux plugin

---
+++ src/redux-remote-data.js
@@ -0,0 +1,19 @@
+export const remoteDataThunkAction = ({
+  actionPrefix = 'REMOTE_DATA',
+}) => {
+    const CHANGE_ACTION = `${actionPrefix}_CHANGE`;
+
+    return (remoteData, ...args) => (dispatch) => {
+      remoteData.onChange = d => dispatch({
+        type: CHANGE_ACTION,
+        remoteData: d
+      });
+
+      return remoteData.fetch(...args).then(d => {
+        dispatch({
+          type: CHANGE_ACTION,
+          remoteData: d
+        });
+      });
+    }
+};
