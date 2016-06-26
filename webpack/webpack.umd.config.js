475441636531416b82fe000b2d5c97419ca5bbab
Adam DiCarlo <adam.dicarlo@gmail.com>
2016-06-26 21:27:47 +0000

    Define NODE_ENV for UMD build

--- webpack/webpack.umd.config.js
+++ webpack/webpack.umd.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: path.join(process.cwd(), 'src', 'index.js'),
@@ -14,5 +15,10 @@ module.exports = {
       loader: 'babel',
       exclude: /node_modules/,
     }]
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 }
