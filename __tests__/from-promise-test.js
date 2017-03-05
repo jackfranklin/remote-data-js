47dada60c7d2e54e8c58938c8eb330eba6dbf86e
Jack Franklin <jack@jackfranklin.net>
2017-03-05 11:10:50 +0000

    Add `from-promise`

---
+++ __tests__/from-promise-test.js
@@ -0,0 +1,29 @@
+import { fromPromise } from '../src/index'
+
+it('can be constructed from a promise and be in the loading state', () => {
+  const onChange = () => {}
+  const prom = new Promise(resolve => resolve({ success: true }))
+  const remoteData = fromPromise(prom, { onChange })
+  expect(remoteData.isPending()).toBe(true)
+})
+
+it('gives the data back when it succeeds', (done) => {
+  const onChange = res => {
+    expect(res.isSuccess()).toEqual(true)
+    expect(res.data).toEqual({ success: true })
+    done()
+  }
+  const prom = new Promise(resolve => resolve({ success: true }))
+  const remoteData = fromPromise(prom, { onChange })
+  expect(remoteData.isPending()).toBe(true)
+})
+
+it('gives the error back when it fails', (done) => {
+  const onChange = res => {
+    expect(res.isFailure()).toEqual(true)
+    expect(res.data).toEqual({ error: true })
+    done()
+  }
+  const prom = new Promise((resolve, reject) => reject({ error: true }))
+  fromPromise(prom, { onChange })
+})
