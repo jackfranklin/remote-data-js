da279625d81369c29b40b94fe30a9127a2caa343
Jack Franklin <jack@jackfranklin.net>
2017-03-05 10:54:08 +0000

    Lint test files

--- __tests__/remote-data-test.js
+++ __tests__/remote-data-test.js
@@ -1,149 +1,149 @@
-import RemoteData from '../src/index';
-import fetchMock from 'fetch-mock';
+import RemoteData from '../src/index'
+import fetchMock from 'fetch-mock'
 
-const mockSuccess = url => fetchMock.mock(url, { success: true });
-const mockError = url => fetchMock.mock(url, 404);
+const mockSuccess = url => fetchMock.mock(url, { success: true })
+const mockError = url => fetchMock.mock(url, 404)
 
 const resetAndMockSuccess = url => {
-  fetchMock.restore();
-  mockSuccess(url);
-};
+  fetchMock.restore()
+  mockSuccess(url)
+}
 
 const resetAndMockError = url => {
-  fetchMock.restore();
-  mockError(url);
-};
+  fetchMock.restore()
+  mockError(url)
+}
 
 const resetAndMockWithResponse = (url, response) => {
-  fetchMock.restore();
-  fetchMock.mock(url, response);
-};
+  fetchMock.restore()
+  fetchMock.mock(url, response)
+}
 
 const makeInstance = (obj = {}) => {
   const args = Object.assign({}, {
     url: 'api.com/1',
-  }, obj);
+  }, obj)
 
-  return new RemoteData(args);
-};
+  return new RemoteData(args)
+}
 
 it('Is NOT_ASKED by default', () => {
-  expect(new RemoteData().isNotAsked()).toBe(true);
-});
+  expect(new RemoteData().isNotAsked()).toBe(true)
+})
 
 describe('defining the URL', () => {
   it('uses a url if it is given one', () => {
-    resetAndMockSuccess('api.com/1');
+    resetAndMockSuccess('api.com/1')
 
     const instance = makeInstance({
       url: x => `api.com/${x}`,
-    });
+    })
 
     return instance.fetch('1').then(() => {
-      expect(fetchMock.called('api.com/1')).toBe(true);
-    });
-  });
+      expect(fetchMock.called('api.com/1')).toBe(true)
+    })
+  })
 
   it('uses a string if given a string', () => {
-    resetAndMockSuccess('api.com/1');
-    const instance = makeInstance();
+    resetAndMockSuccess('api.com/1')
+    const instance = makeInstance()
 
     return instance.fetch('1').then(() => {
-      expect(fetchMock.called('api.com/1')).toBe(true);
-    });
-  });
-});
+      expect(fetchMock.called('api.com/1')).toBe(true)
+    })
+  })
+})
 
 it('calls onChange twice when there is a successful request', () => {
-  resetAndMockSuccess('api.com/1');
-  const onChange = jest.fn();
-  const instance = makeInstance({ onChange });
+  resetAndMockSuccess('api.com/1')
+  const onChange = jest.fn()
+  const instance = makeInstance({ onChange })
 
   return instance.fetch().then(() => {
-    expect(onChange.mock.calls[0][0].isPending()).toBe(true);
-    expect(onChange.mock.calls[1][0].isSuccess()).toBe(true);
-  });
-});
+    expect(onChange.mock.calls[0][0].isPending()).toBe(true)
+    expect(onChange.mock.calls[1][0].isSuccess()).toBe(true)
+  })
+})
 
 it('calls onChange twice for a failed request', () => {
-  resetAndMockError('api.com/1');
-  const onChange = jest.fn();
-  const instance = makeInstance({ onChange });
+  resetAndMockError('api.com/1')
+  const onChange = jest.fn()
+  const instance = makeInstance({ onChange })
 
   return instance.fetch().then(() => {
-    expect(onChange.mock.calls[0][0].isPending()).toBe(true);
-    expect(onChange.mock.calls[1][0].isFailure()).toBe(true);
-  });
-});
+    expect(onChange.mock.calls[0][0].isPending()).toBe(true)
+    expect(onChange.mock.calls[1][0].isFailure()).toBe(true)
+  })
+})
 
 it('parses as JSON by default', () => {
-  resetAndMockWithResponse('api.com/1', { some: 'json' });
-  const instance = makeInstance();
+  resetAndMockWithResponse('api.com/1', { some: 'json' })
+  const instance = makeInstance()
 
   return instance.fetch().then(result => {
-    expect(result.data).toEqual({ some: 'json' });
-  });
-});
+    expect(result.data).toEqual({ some: 'json' })
+  })
+})
 
 it('allows a custom parser function to be provided', () => {
-  resetAndMockWithResponse('api.com/1', 'Hello World');
-  const instance = makeInstance({ parse: x => x.text() });
+  resetAndMockWithResponse('api.com/1', 'Hello World')
+  const instance = makeInstance({ parse: x => x.text() })
 
   return instance.fetch().then(result => {
-    expect(result.data).toEqual('Hello World');
-  });
-});
+    expect(result.data).toEqual('Hello World')
+  })
+})
 
 it('provides `response` to allow access to the raw HTTP response', () => {
-  resetAndMockWithResponse('api.com/1', { some: 'json' });
-  const instance = makeInstance();
+  resetAndMockWithResponse('api.com/1', { some: 'json' })
+  const instance = makeInstance()
 
   return instance.fetch().then(result => {
-    expect(result.data).toEqual({ some: 'json' });
-    expect(result.response.status).toEqual(200);
-  });
-});
+    expect(result.data).toEqual({ some: 'json' })
+    expect(result.response.status).toEqual(200)
+  })
+})
 
 it('throws if you access the response when the request is not finished', () => {
-  const instance = makeInstance();
+  const instance = makeInstance()
 
   expect(() => instance.response).toThrowError(
     /Cannot get response for request that hasn't finished/
-  );
-});
+  )
+})
 
 it('throws if you try to access the data before it has been fetched', () => {
-  const instance = makeInstance();
+  const instance = makeInstance()
 
   expect(() => instance.data).toThrowError(
     /Cannot get data for request that hasn't finished/
-  );
-});
+  )
+})
 
 describe('the case method', () => {
   it('first calls the NotAsked callback', () => {
-    const instance = makeInstance();
+    const instance = makeInstance()
 
-    const notAsked = jest.fn();
-    const otherFn = jest.fn();
+    const notAsked = jest.fn()
+    const otherFn = jest.fn()
 
     instance.case({
       NotAsked: notAsked,
       Pending: otherFn,
       Failure: otherFn,
       Success: otherFn,
-    });
+    })
 
-    expect(notAsked).toBeCalled();
-    expect(otherFn).not.toBeCalled();
-  });
+    expect(notAsked).toBeCalled()
+    expect(otherFn).not.toBeCalled()
+  })
 
   it('calls the success callback when data has been fetched', () => {
-    resetAndMockSuccess('api.com/1');
-    const instance = makeInstance();
+    resetAndMockSuccess('api.com/1')
+    const instance = makeInstance()
 
-    const successFn = jest.fn();
-    const otherFn = jest.fn();
+    const successFn = jest.fn()
+    const otherFn = jest.fn()
 
     return instance.fetch().then(result => {
       result.case({
@@ -151,47 +151,47 @@ describe('the case method', () => {
         Pending: otherFn,
         Failure: otherFn,
         Success: successFn,
-      });
+      })
 
-      expect(successFn).toBeCalledWith({ success: true });
-      expect(otherFn).not.toBeCalled();
-    });
-  });
+      expect(successFn).toBeCalledWith({ success: true })
+      expect(otherFn).not.toBeCalled()
+    })
+  })
 
   it('calls the pending callback when the request is in motion', () => {
-    resetAndMockSuccess('api.com/1');
+    resetAndMockSuccess('api.com/1')
 
-    const pendingFn = jest.fn();
-    const otherFn = jest.fn();
+    const pendingFn = jest.fn()
+    const otherFn = jest.fn()
 
-    let count = 0;
+    let count = 0
     const instance = new RemoteData({
       url: 'api.com/1',
       onChange(remoteData) {
-        if (count++ > 0) return;
+        if (count++ > 0) return
 
         remoteData.case({
           NotAsked: otherFn,
           Pending: pendingFn,
           Failure: otherFn,
           Success: otherFn,
-        });
+        })
 
-        expect(pendingFn).toBeCalled();
-        expect(otherFn).not.toBeCalled();
+        expect(pendingFn).toBeCalled()
+        expect(otherFn).not.toBeCalled()
       },
-    });
+    })
 
-    return instance.fetch();
-  });
+    return instance.fetch()
+  })
 
   it('calls the failure callback when the request failed', () => {
-    resetAndMockError('api.com/1');
+    resetAndMockError('api.com/1')
 
-    const instance = makeInstance();
+    const instance = makeInstance()
 
-    const failedFn = jest.fn();
-    const otherFn = jest.fn();
+    const failedFn = jest.fn()
+    const otherFn = jest.fn()
 
     return instance.fetch().then(result => {
       result.case({
@@ -199,36 +199,36 @@ describe('the case method', () => {
         Pending: otherFn,
         Failure: failedFn,
         Success: otherFn,
-      });
+      })
 
-      expect(failedFn).toBeCalled();
-      expect(otherFn).not.toBeCalled();
-    });
-  });
+      expect(failedFn).toBeCalled()
+      expect(otherFn).not.toBeCalled()
+    })
+  })
 
   it('will call the default callback if none match', () => {
-    resetAndMockError('api.com/1');
-    const handler = jest.fn();
-    const instance = makeInstance();
+    resetAndMockError('api.com/1')
+    const handler = jest.fn()
+    const instance = makeInstance()
 
     instance.case({
       _: handler,
-    });
+    })
 
-    expect(handler).toBeCalled();
-  });
+    expect(handler).toBeCalled()
+  })
 
   it('copies the onChange event over when a new remote data instance is created', () => {
-    const onChange = jest.fn();
+    const onChange = jest.fn()
     const instance = makeInstance({
       onChange,
-    });
+    })
 
-    resetAndMockSuccess('api.com/1');
+    resetAndMockSuccess('api.com/1')
 
     return instance.fetch().then((newInstance) => {
-      expect(onChange.mock.calls.length).toBe(2);
-      expect(newInstance.onChange).toBe(onChange);
-    });
-  });
-});
+      expect(onChange.mock.calls.length).toBe(2)
+      expect(newInstance.onChange).toBe(onChange)
+    })
+  })
+})
